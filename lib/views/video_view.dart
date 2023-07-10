17bf2587b651dcad07a9001f3f2a04f1623bee71
Joel G Mathew <joel@geekmaze.com>
2023-07-10 13:59:37 +0530

    Added sharing files, and renaming

--- lib/views/video_view.dart
+++ lib/views/video_view.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenVideoView extends StatefulWidget {
@@ -13,6 +17,7 @@ class FullScreenVideoView extends StatefulWidget {
 
 class _FullScreenVideoViewState extends State<FullScreenVideoView> {
   late VideoPlayerController _controller;
+  late TextEditingController _filenameController;
   final ValueNotifier<double> _progress = ValueNotifier<double>(0);
 
   @override
@@ -50,39 +55,162 @@ class _FullScreenVideoViewState extends State<FullScreenVideoView> {
                       left: 0,
                       right: 0,
                       // top: 0,
-                      child: Row(
+                      child: Column(
                         children: [
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _controller.value.isPlaying
-                                    ? _controller.pause()
-                                    : _controller.play();
-                              });
-                            },
-                            icon: Icon(
-                              _controller.value.isPlaying
-                                  ? Icons.pause
-                                  : Icons.play_arrow,
-                              color: Colors.blue[900],
+                          Text(
+                            File(widget.videoPath).path.split('/').last,
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
                             ),
                           ),
-                          Expanded(
-                            child: ValueListenableBuilder<double>(
-                              valueListenable: _progress,
-                              builder: (context, value, child) {
-                                return Slider(
-                                  value: value,
-                                  min: 0.0,
-                                  max: _controller.value.duration.inSeconds
-                                      .toDouble(),
-                                  onChanged: (double newValue) {
-                                    _controller.seekTo(
-                                        Duration(seconds: newValue.toInt()));
-                                  },
+                          FutureBuilder(
+                            future: File(widget.videoPath).lastModified(),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<DateTime> snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.done) {
+                                // The Future is complete and data is available.
+                                String formattedDate =
+                                    DateFormat('dd, MMMM yyyy')
+                                        .format(snapshot.data!);
+                                return Text(
+                                  formattedDate,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
                                 );
-                              },
+                              } else if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                // The Future is still running.
+                                return CircularProgressIndicator();
+                              } else {
+                                // Something went wrong.
+                                return Text('Error: ${snapshot.error}');
+                              }
+                            },
+                          ),
+                          VideoProgressIndicator(
+                            _controller,
+                            allowScrubbing: true,
+                            colors: VideoProgressColors(
+                              playedColor: Colors.blue[900]!,
+                              bufferedColor: Colors.blue[900]!.withOpacity(0.5),
+                              backgroundColor: Colors.grey[300]!,
                             ),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 5,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _controller.value.isPlaying
+                                        ? _controller.pause()
+                                        : _controller.play();
+                                  });
+                                },
+                                icon: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: Icon(
+                                    _controller.value.isPlaying
+                                        ? Icons.pause
+                                        : Icons.play_arrow,
+                                    color: Colors.blue[900],
+                                    size: 40,
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                icon: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: Icon(
+                                    _controller.value.volume == 0
+                                        ? Icons.volume_off
+                                        : Icons.volume_up,
+                                    color: _controller.value.volume == 0
+                                        ? Colors.red
+                                        : Colors.blue[900],
+                                  ),
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    if (_controller.value.volume == 0) {
+                                      _controller.setVolume(1);
+                                    } else {
+                                      _controller.setVolume(0);
+                                    }
+                                  });
+                                },
+                              ),
+                              IconButton(
+                                icon: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: Icon(Icons.share),
+                                ), // Three vertically aligned dots.
+                                onPressed: () {
+                                  Share.shareXFiles(
+                                    [XFile(widget.videoPath)],
+                                    text: 'Sharing video...',
+                                  );
+                                  // Handle the button press here.
+                                },
+                              ),
+                              IconButton(
+                                icon: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: Icon(
+                                      Icons.drive_file_rename_outline_sharp),
+                                ), // Three vertically aligned dots.
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text('Rename File'),
+                                        content: TextField(
+                                          controller: _filenameController
+                                            ..text = File(widget.videoPath)
+                                                .path
+                                                .split('/')
+                                                .last, // prefill with filename
+                                          decoration: InputDecoration(
+                                              hintText: "Enter new name"),
+                                        ),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child: Text('Rename'),
+                                            onPressed: () async {
+                                              String newPath = widget.videoPath
+                                                  .replaceFirst(
+                                                      RegExp(r'[^/]*$'),
+                                                      _filenameController.text);
+                                              await File(widget.videoPath)
+                                                  .rename(newPath);
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                              IconButton(
+                                icon: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: Icon(Icons.more_vert),
+                                ), // Three vertically aligned dots.
+                                onPressed: () {
+                                  // Handle the button press here.
+                                },
+                              ),
+                            ],
                           ),
                         ],
                       ),
